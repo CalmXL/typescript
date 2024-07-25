9a963ac6bfbe18151025cdac7d487b6317b46d28
xl <xl18845898715@gmail.com>
2024-07-25 08:35:59 +0800

    feat: 模板字符串

--- src/14_basic/index.ts
+++ src/14_basic/index.ts
@@ -32,5 +32,61 @@ type sayHello<T extends Types> = `hello, ${T}`;
 let s1: sayHello<Types> = `hello, ${ undefined }`;
 console.log(s1);
 
+type R1 = sayHello<'xl'>
+type R2 = sayHello<30>;
+type R3 = sayHello<number>;
+
+// 所有基础类型的模板字符串都是字面量类型的父类型
+type Iflag = R2 extends R3 ? true : false;
+
+// 将对象的属性进行重命名操作
+
+type Person = {
+  name: string;
+  age: number;
+  address: string;
+}
+
+type P<T> = { // 模板字符串进行变量的重命名
+  [K in keyof T as `r_${K & string}`]: T[K];
+}
+
+type ReType = P<Person>;
+
+
+// 字符串可以支持工具类型 UpperCase LowerCase CapitalCase UpCapitalCase
+let person: Person = {
+  name: 'xl',
+  age: 20,
+  address: 'sh'
+}
+
+type WithGetter<T> = {
+  [K in keyof T as `get${Capitalize<K & string>}`]?: () => T[K]
+}
+type Compute<T> = { [K in keyof T]: T[K]}
+type WithGetterType = Compute<WithGetter<Person>>
+let personGetter: WithGetter<Person> = {
+  getName() {
+    return person.name;
+  },
+
+  getAge() {
+    return person.age;
+  },
+
+  getAddress () {
+    return person.address;
+  }
+
+}
+
+// 根据模式匹配符来取类型 
+// infer 可以进行位置推断
+// 可以推断数组 | 元组 | string
+
+type GetNameFirstChar<T> = T extends `${infer F} ${infer X}` ? F : never;
+
+type FirstChar = GetNameFirstChar<'xu lei'>;
 
 export {};

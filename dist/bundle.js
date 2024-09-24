4000e79b355f7c294e2858bec262408348998b45
xulei <xl18845898715@gmail.com>
2024-09-24 18:04:21 +0800

    feat: update config package

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,9 +1,51 @@
 
 (function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
 (function () {
-	'use strict';
+  'use strict';
 
+  class Axios {
+      request(config) {
+          // 发送请求需要对我们的配置进行合并，进行修改等操作
+          // 1. 对配置进行合并， 默认值
+          // 2. 拦截器
+          // 3. 发送请求
+          return this.dispatchRequest(config);
+      }
+      dispatchRequest(config) {
+          return new Promise((resolve, reject) => {
+              new XMLHttpRequest();
+          });
+      }
+  }
 
+  function createInstance() {
+      const context = new Axios();
+      // 保证 this 永远指向 类的实例
+      const instance = Axios.prototype.request.bind(context);
+      return instance;
+  }
+  const axios = createInstance();
+
+  // import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+  // 基础路径
+  const baseURL = 'http://localhost:8080';
+  let person = {
+      name: 'xl',
+      age: 28,
+  };
+  let requestConfig = {
+      url: baseURL + '/get',
+      method: 'GET',
+      params: person,
+  };
+  // 希望的返回值
+  axios(requestConfig)
+      .then((response) => {
+      console.log(response.data);
+  })
+      .catch((error) => {
+      console.log(error);
+  });
 
 })();
 //# sourceMappingURL=bundle.js.map
--- rollup.config.js
+++ rollup.config.js
@@ -5,32 +5,32 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 
-const __filename = fileURLToPath(import.meta.url)
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 console.log(__dirname, __filename);
 // => /Users/xulei/code/js++/i/zhufeng/ts
 // => /Users/xulei/code/js++/i/zhufeng/ts/rollup.config.js
 
 export default {
-  input: resolve(__dirname, 'src/16_basic/index.ts'),
+  input: resolve(__dirname, 'src/18_axios/index.ts'),
   output: {
     format: 'iife',
     file: resolve(__dirname, 'dist/bundle.js'),
-    sourcemap: true
+    sourcemap: true, // 当前代码在浏览器中使用
   },
   plugins: [
     nodeResolve({
-      extensions: ['.js', '.ts']
+      extensions: ['.js', '.ts'],
+      browser: true,
     }),
     ts({
-      tsconfig: resolve(__dirname, 'tsconfig.json')
+      tsconfig: resolve(__dirname, 'tsconfig.json'),
     }),
     serve({
       port: 3000,
       openPage: '/public/index.html',
       // open: true
     }),
-    livereload()
+    livereload(),
   ],
-
-}
+};
